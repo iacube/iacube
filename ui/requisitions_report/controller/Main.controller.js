d0f24959b752e5acc5be3b1de0ca0a84df2885fd
Stanislau Razhanski <stanislau.razhanski@sap.com>
2017-05-17 12:24:52 +0000

    requisitions report template, data retrieval methods

---
+++ ui/requisitions_report/controller/Main.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,JSONModel) {
+	"use strict";
+
+	return Controller.extend("requisitions_report.controller.Main", {
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Used to instantiate a special view model and handle busy state of whole app
+		 * @memberOf com.gs.sls.bpo.ui.mod.chg_lang.view.Main
+		 * @public
+		 */
+		onInit: function() {
+
+
+		}
+	});
+});
