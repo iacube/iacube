111cc2385e4fbaef6ea2b66c0d0ecdf095ee37ec
Stanislau_Razhanski <Stanislau_Razhanski@EPBYMINW3527.minsk.epam.com>
2017-06-01 17:20:25 +0300

    Additional changes for candidates table

--- ui/requisitions_report/Component.js
+++ ui/requisitions_report/Component.js
@@ -12,7 +12,7 @@ sap.ui.define([
 		},
 		
 		constructor: function(){
-			jQuery.sap.registerModulePath("iacube.ui.common", "/ui/common");
+			jQuery.sap.registerModulePath("iacube.ui.common", "../common");
 			UIComponent.prototype.constructor.apply(this, arguments);
 		},
 
